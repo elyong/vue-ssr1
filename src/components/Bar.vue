3fb0739e27e75f916b7a1e3971e9cb5587856939
王健 <1162331666@qq.com>
2020-09-28 18:00:28 +0800

    3

--- src/components/Bar.vue
+++ src/components/Bar.vue
@@ -1,8 +1,29 @@
 <template>
   <div class="bar">
-    <h1>Bar Component</h1>
+    <h1 @click="onHandleClick">Bar Component</h1>
+    <h2>异步Ajax数据：</h2>
+    <span>{{ msg }}</span>
   </div>
 </template>
+
+<script>
+const fetchInitialData = ({ store }) => {
+  store.dispatch('fetchBar')
+}
+export default {
+  asyncData: fetchInitialData,
+
+  methods: {
+    onHandleClick() {
+      alert('bar')
+    }
+  },
+
+  mounted() {
+    let store = this.$store
+  }
+}
+</script>
 
 <style>
 .bar {
