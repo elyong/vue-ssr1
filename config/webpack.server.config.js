07a3e519b21e39f3804a2f214a8dbec7929b37ef
王健 <1162331666@qq.com>
2020-09-29 17:57:55 +0800

    4

--- config/webpack.server.config.js
+++ config/webpack.server.config.js
@@ -1,17 +1,22 @@
 const path = require('path');
 const merge = require('webpack-merge');
+const nodeExternals = require('webpack-node-externals')
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const VueSSRServerPlugin = require('vue-server-renderer/server-plugin')
 const base = require('./webpack.base.config');
 
 module.exports = merge(base, {
   target: 'node',
+  devtool: '#source-map',
   entry: {
     server: path.resolve(__dirname, '../src/entry-server.js')
   },
+  externals: [nodeExternals()],
   output: {
     libraryTarget: 'commonjs2'
   },
   plugins: [
+    new VueSSRServerPlugin(),
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, '../src/index.ssr.html'),
       filename: 'index.ssr.html',
