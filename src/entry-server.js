a377ddacb00113a4ed288c3f41477c832d614314
王健 <1162331666@qq.com>
2020-09-29 11:36:47 +0800

    3 done

--- src/entry-server.js
+++ src/entry-server.js
@@ -1,28 +1,28 @@
-import { resolve } from '../config/webpack.base.config.js';
 import { createApp } from './app.js';
 
 export default context => {
   return new Promise((resolve, reject) => {
-    const { app, store, App } = createApp()
-    
-    let components = App.components
-    let asyncDataPromisesFns = []
+    const { app, store, App } = createApp();
 
+    let components = App.components;
+    let asyncDataPromiseFns = [];
+  
     Object.values(components).forEach(component => {
       if (component.asyncData) {
-        asyncDataPromisesFns.push(component.asyncData({ store }))
+        asyncDataPromiseFns.push(component.asyncData({ store }));
       }
-    })
-
-    Promise.all(asyncDataPromisesFns).then(result => {
-      context.state = store.state
-
-      console.log(222)
-      console.log(store.state)
-      console.log(context.state)
-      console.log(context)
-
-      resolve(app)
-    }, reject)
-  })
+    });
+  
+    Promise.all(asyncDataPromiseFns).then((result) => {
+      // 当使用 template 时，context.state 将作为 window.__INITIAL_STATE__ 状态，自动嵌入到最终的 HTML 中
+      context.state = store.state;
+  
+      console.log(222);
+      console.log(store.state);
+      console.log(context.state);
+      console.log(context);
+  
+      resolve(app);
+    }, reject);
+  });
 }
