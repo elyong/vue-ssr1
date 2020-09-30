7b3a8efca07df76dd256271e8e3e8537b47c4009
王健 <1162331666@qq.com>
2020-09-30 16:08:02 +0800

    5

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,24 @@
 <template>
   <div id='app'>
-    <Foo></Foo>
-    <Bar></Bar>
+    <router-link to="/bar">Goto Bar</router-link>
+    <router-link to="/foo">Goto Foo</router-link>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import Foo from './components/Foo.vue';
-import Bar from './components/Bar.vue';
-
 export default {
-  components: {
-    Foo, Bar
+  beforeCreate() {
+    console.log('App.vue beforeCreate')
+  },
+  created() {
+    console.log('App.vue created')
+  },
+  beforeMount() {
+    console.log('App.vue beforeMount')
+  },
+  mounted() {
+    console.log('App.vue mounted')
   }
 }
 </script>
--- src/entry-server.js
+++ src/entry-server.js
@@ -2,27 +2,35 @@ import { createApp } from './app.js';
 
 export default context => {
   return new Promise((resolve, reject) => {
-    const { app, store, App } = createApp();
+    const { app, store, router, App } = createApp();
 
-    let components = App.components;
-    let asyncDataPromiseFns = [];
-  
-    Object.values(components).forEach(component => {
-      if (component.asyncData) {
-        asyncDataPromiseFns.push(component.asyncData({ store }));
-      }
-    });
-  
-    Promise.all(asyncDataPromiseFns).then((result) => {
-      // 当使用 template 时，context.state 将作为 window.__INITIAL_STATE__ 状态，自动嵌入到最终的 HTML 中
-      context.state = store.state;
-  
-      console.log(222);
-      console.log(store.state);
-      console.log(context.state);
-      console.log(context);
-  
-      resolve(app);
-    }, reject);
+    router.push(context.url)
+
+    router.onReady(() => {
+        const matchedComponents = router.getMatchedComponents()
+
+        console.log(context.url)
+        console.log(matchedComponents)
+
+        if (!matchedComponents.length) {
+            return reject({ code: 404})
+        }
+      
+        Promise.all(matchedComponents.map(component => {
+            if (component.asyncData) {
+                return component.asyncData({ store })
+            }
+        })).then((result) => {
+          // 当使用 template 时，context.state 将作为 window.__INITIAL_STATE__ 状态，自动嵌入到最终的 HTML 中
+          context.state = store.state;
+      
+          console.log(222);
+          console.log(store.state);
+          console.log(context.state);
+          console.log(context);
+      
+          resolve(app);
+        }, reject);
+    })
   });
 }
